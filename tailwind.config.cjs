6cb2af8c35de17332dd4b36269cdeda11c7ae859
Fidan Ademi <fidomax07@gmail.com>
2022-08-30 07:18:44 +0200

    Install and configure prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,17 @@ const colors = require("tailwindcss/colors")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        // This is just an example of how we can get some of the colors from the 
+        // This is just an example of how we can get some of the colors from the
         // Tailwind's default color palette, otherwise the "orange" and "teal"
         // are already added again to Tailwind v3 since they were not in v2.
         orange: colors.orange,
         teal: colors.teal,
-        "light-blue": colors.sky
-      }
+        "light-blue": colors.sky,
+      },
     },
   },
   plugins: [],
